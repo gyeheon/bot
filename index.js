6eb0df80c1c4953b135da11e6bc59c5709654a02
gyeheon <gyeheon1234@gmail.com>
2020-09-18 18:30:19 +0900

    dd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '아무것도 안.' }, status: 'online' })
+  client.user.setPresence({ game: { name: '아무것도 안' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
